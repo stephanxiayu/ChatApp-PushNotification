258a36bfa1db39ae59e8d952d4a33513c06488b8
Stephan Granzow <stephangranzow@MBP-von-Stephan.fritz.box>
2022-03-19 16:43:16 +0100

    Changes to be committed: 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:chatapp_pushnotification/allProvider/auth_provider.dart';
+import 'package:chatapp_pushnotification/constants/app_constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 bool isWhite = false;
@@ -19,15 +24,26 @@ class MyApp extends StatelessWidget {
   final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
 
-  MyApp({required this.prefs});
+  MyApp({Key? key, required this.prefs}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'iChat App',
-      theme: ThemeData.dark(),
-      home: Scaffold(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(
+            create: (_) => AuthProvider(
+                  firebaseAuth: FirebaseAuth.instance,
+                  googleSignIn: GoogleSignIn(),
+                  prefs: this.prefs,
+                  firebaseFirestore: this.firebaseFirestore,
+                )),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: AppConstants.appTitle,
+        theme: ThemeData.dark(),
+        home: Scaffold(),
+      ),
     );
   }
 }
